208038c37bf442811106be9031548b73381472f3
Kostis Kosmidis <25617322+kostiskosmidis@users.noreply.github.com>
2020-07-05 11:54:04 +0300

    Add files via upload

---
+++ Math.java
@@ -0,0 +1,14 @@
+public class Math {
+    public static void main(String[] args) {
+        double x = 3.45;
+        double y = java.lang.Math.ceil(x);
+        double z = java.lang.Math.floor(x);
+        double k = java.lang.Math.round(x);
+        double l = java.lang.Math.abs(x);
+        System.out.println("Ceil of x: " + y);
+        System.out.println("Floor of x: " + z);
+        System.out.println("Round of x: " + k);
+        System.out.println("Abs of x: " + l);
+    }
+    
+}

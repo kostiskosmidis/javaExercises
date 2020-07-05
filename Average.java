03599fed511fadd4fd18bf69d3aea667d56b8660
Kostis Kosmidis <25617322+kostiskosmidis@users.noreply.github.com>
2020-07-05 20:04:54 +0300

    Update Average.java

--- Average.java
+++ Average.java
@@ -2,7 +2,7 @@ public class  Average{
  public static void main(String[] args){
    int firstArg = Integer.parseInt(args[0]);
    int secondArg = Integer.parseInt(args[1]);
-   int thirdArg = Integer.parseInt(args[1]);
+   int thirdArg = Integer.parseInt(args[2]);
    int average;
    average = (firstArg + secondArg + thirdArg) / 3;
    System.out.println("Average is: " + average);

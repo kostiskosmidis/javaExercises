fd9edcf658517c43fe99f6f3353a19b539132e0f
Kostis Kosmidis <25617322+kostiskosmidis@users.noreply.github.com>
2020-07-05 13:13:18 +0300

    Update Math.java

--- Math.java
+++ Math.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
 public class Math {
     public static void main(String[] args) {
-        double x = 3.45;
-        double y = java.lang.Math.ceil(x);
-        double z = java.lang.Math.floor(x);
-        double k = java.lang.Math.round(x);
-        double l = java.lang.Math.abs(x);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter double number");
+        double num = sc.nextDouble();
+        double y = java.lang.Math.ceil(num);
+        double z = java.lang.Math.floor(num);
+        double k = java.lang.Math.round(num);
+        double l = java.lang.Math.abs(num);
         System.out.println("Ceil of x: " + y);
         System.out.println("Floor of x: " + z);
         System.out.println("Round of x: " + k);
